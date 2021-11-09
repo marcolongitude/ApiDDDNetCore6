48a7ca55d78f141f44df911953851c3d03e6f0a8
Marco Aurelio <marcocpdti@gmail.com>
2021-11-09 00:43:03 -0300

    Adicionar arquivos de projeto.

---
+++ Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+    internal class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        
+        public MyContext CreateDbContext(string[] args)
+        {
+            //var connectionString = "Server=172.19.192.1;Data Source=dbApi;Initial Catalog=dbApi;User ID=sa;Password=adminmagti@1981";
+            var connectionString = "Server=localhost,1433;Database=dbApi;User ID=sa;Password=adminmagti@1981;Trusted_Connection=false;MultipleActiveResultSets=true";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Api.Data/Context/MyContext.cs
@@ -0,0 +1,25 @@
+﻿using Api.Domain.Entities;
+using Data.Mapping;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+    public class MyContext: DbContext
+    {
+        public DbSet<UserEntity>? Users { get; set; }
+
+        public MyContext(DbContextOptions<MyContext> options) : base(options) { }
+
+        protected override void OnModelCreating (ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<UserEntity>(new UserMap().Configure);
+        }
+
+    }
+}
